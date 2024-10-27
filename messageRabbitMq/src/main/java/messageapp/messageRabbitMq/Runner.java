44ad79ee9550623cfc5e190acb463418f0eb788c
Ojo Olorunfemi Adewale <adewaleolorunfemi74@gmail.com>
2024-10-27 20:21:41 +0100

    updated

--- messageRabbitMq/src/main/java/messageapp/messageRabbitMq/Runner.java
+++ messageRabbitMq/src/main/java/messageapp/messageRabbitMq/Runner.java
@@ -14,6 +14,7 @@ public class Runner implements CommandLineRunner{
 	private Runner (Receiver receiver, RabbitTemplate rabbitTemplate) {
 		this.receiver = receiver;
 		this.rabbitTemplate = rabbitTemplate;
+		
 	}
 
 	@Override
